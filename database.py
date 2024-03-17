2bcaa54c319fbb6bef06c734c887b4648f13f79d
MilanKutnogorskiy <kutnogorskiy77@gmail.com>
2024-03-17 21:54:27 +0300

    step_03

--- database.py
+++ database.py
@@ -1,17 +1,16 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
-from sqlalchemy.orm import sessionmaker, DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
-engine = create_async_engine(
-    "sqlite+aiosqlite:///tasks.db"
-)
-
+engine = create_async_engine("sqlite+aiosqlite:///tasks.db")
 new_session = async_sessionmaker(engine, expire_on_commit=False)
+
 
 class Model(DeclarativeBase):
     pass
 
+
 # Модель задачи
-class Task(Model):
+class TaskOrm(Model):
     __tablename__ = "tasks"
 
     id: Mapped[int] = mapped_column(primary_key=True)
@@ -29,12 +28,13 @@ class Employee(Model):
     name: Mapped[str]
     position: Mapped[str]
 
-# Функция создания таблиц
 async def create_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(Model.metadata.create_all)
+
+
+async def delete_tables():
     async with engine.begin() as conn:
         await conn.run_sync(Model.metadata.drop_all)
 
-# Функция удаления таблиц
-async def delete_tables():
-    async with engine.begin() as conn:
-        await conn.run_sync(Model.metadata.create_all)
+
